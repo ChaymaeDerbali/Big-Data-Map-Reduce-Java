3b23922acdf1561c3de1c5a991f0a3b1f51cc77e
ChaymaeDerbali <chaymae.derbali@yahoo.fr>
2017-03-08 06:18:15 -0800

    Fourth commit

---
+++ Big-Data-Map-Reduce-Java/Activite4/src/StubReducer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class StubReducer extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+
+
+
+@Override
+public void reduce(Text key, Iterator<DoubleWritable> values,
+		OutputCollector<Text, DoubleWritable> output, Reporter arg3)
+		throws IOException {
+
+	  
+	  double sum=0;
+	  while(values.hasNext())
+	  {
+		  sum+=values.next().get();
+	  }
+      output.collect(key, new DoubleWritable(sum));
+}
+}
